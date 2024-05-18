d640493505f00961a9255b99e394f332f0713042
Adrian <adrian.essig@outlook.com>
2024-05-18 23:06:06 +0200

    Added TennisClubId to AdminRegisterCommand

--- ClubService.Application/Commands/AdminRegisterCommand.cs
+++ ClubService.Application/Commands/AdminRegisterCommand.cs
@@ -1,8 +1,10 @@
 namespace ClubService.Application.Commands;
 
-public class AdminRegisterCommand(string username, string firstName, string lastName)
+public class AdminRegisterCommand(string username, string firstName, string lastName, string tennisClubId)
 {
     public string Username { get; } = username;
     public string FirstName { get; } = firstName;
     public string LastName { get; } = lastName;
+    
+    public string TennisClubId { get; } = tennisClubId;
 }
