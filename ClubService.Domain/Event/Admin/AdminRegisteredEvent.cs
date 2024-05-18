b5c75f21aa6d40894a1f549149e5929fc3c29861
Adrian <adrian.essig@outlook.com>
2024-05-18 23:06:28 +0200

    Changed Id to AdminId in Event

--- ClubService.Domain/Event/Admin/AdminRegisteredEvent.cs
+++ ClubService.Domain/Event/Admin/AdminRegisteredEvent.cs
@@ -3,13 +3,13 @@ using ClubService.Domain.Model.ValueObject;
 namespace ClubService.Domain.Event.Admin;
 
 public class AdminRegisteredEvent(
-    AdminId id,
+    AdminId adminId,
     string username,
     FullName name,
     TennisClubId tennisClubId,
     bool isDeleted) : IAdminDomainEvent
 {
-    public AdminId Id { get; } = id;
+    public AdminId AdminId { get; } = adminId;
     public string Username { get; } = username;
     public FullName Name { get; } = name;
     public TennisClubId TennisClubId { get; } = tennisClubId;
