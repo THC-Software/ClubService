7600046a8fa0b1d27d26d8d8bf6241fd7ef2a585
Marco Prescher <mpr8243@students.fhv.at>
2024-05-21 12:15:38 +0200

    Added additional ConflictException constructor

--- ClubService.Application/Api/Exceptions/ConflictException.cs
+++ ClubService.Application/Api/Exceptions/ConflictException.cs
@@ -2,6 +2,10 @@ namespace ClubService.Application.Api.Exceptions;
 
 public class ConflictException : Exception
 {
+    public ConflictException(string? message) : base(message)
+    {
+    }
+    
     public ConflictException(string? message, Exception? innerException) : base(message, innerException)
     {
     }
