b36305db592bc177e305571ec4904a9c86f7d5bf
Adrian <adrian.essig@outlook.com>
2024-05-30 13:58:12 +0200

    Added BackgroundServices that handles calling the eventreader to consume messages

---
+++ ClubService.Application/EventProcessingService.cs
@@ -0,0 +1,19 @@
+﻿using ClubService.Infrastructure.Api;
+using Microsoft.Extensions.Hosting;
+
+namespace ClubService.Application;
+
+public class EventProcessingService(IEventReader eventReader) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            await eventReader.ConsumeMessagesAsync();
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Redis Event Reader stopped!");
+        }
+    }
+}
