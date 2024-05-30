b7464b967a24c270c185b6335eb65ad526cb23fd
Adrian <adrian.essig@outlook.com>
2024-05-30 13:59:52 +0200

    Redis EventReader is now able to read events from the redis queue. Parsing is still missing

--- ClubService.Infrastructure/RedisEventReader.cs
+++ ClubService.Infrastructure/RedisEventReader.cs
@@ -1,52 +1,67 @@
-// using ClubService.Infrastructure.Api;
-// using Microsoft.Extensions.DependencyInjection;
-// using StackExchange.Redis;
-//
-// namespace ClubService.Infrastructure;
-//
-// public class RedisEventReader : IEventReader
-// {
-//     private readonly CancellationToken _cancellationToken;
-//     private const string StreamName = "club_service_events.public.DomainEvent";
-//     private IDatabase _db;
-//
-//     public RedisEventReader()
-//     {
-//         _cancellationToken = new CancellationTokenSource().Token;
-//         var muxer = ConnectionMultiplexer.Connect("localhost");
-//         _db = muxer.GetDatabase();
-//         
-//     }
-//
-//     public async Task ExecuteAsync()
-//     {
-//         if (!(await _db.KeyExistsAsync(StreamName)) ||
-//             (await _db.StreamGroupInfoAsync(StreamName)).All(x=>x.Name!=GroupName))
-//         {
-//             await _db.StreamCreateConsumerGroupAsync(StreamName, GroupName, "0-0", true);
-//         }
-//         
-//         var id = string.Empty;
-//         while (!_cancellationToken.IsCancellationRequested)
-//         {
-//             if (!string.IsNullOrEmpty(id))
-//             {
-//                 await _db.StreamAcknowledgeAsync(StreamName, GroupName, id);
-//                 id = string.Empty;
-//             }
-//             var result = await _db.StreamReadGroupAsync(StreamName, GroupName, "pos-member", ">", 1);
-//             if (result.Any())
-//             {
-//                 var streamEntry = result.First();
-//                 id = streamEntry.Id;
-//                 var parsedEvent = ParseEvent(streamEntry);
-//                 if (parsedEvent == null)
-//                     continue;
-//                 await memberRepository.UpdateEntityAsync(parsedEvent);
-//             }
-//             await Task.Delay(1000);
-//         }
-//     }
-//
-//
-// }
+using ClubService.Infrastructure.Api;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace ClubService.Infrastructure;
+
+public class RedisEventReader : IEventReader
+{
+    private readonly CancellationToken _cancellationToken;
+    private const string StreamName = "club_service_events.public.DomainEvent";
+    private const string GroupName = "club_service_events.domain.events.group";
+    private readonly IDatabase _db;
+
+    public RedisEventReader(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("{RedisEventReader}: constructor called start");
+        _cancellationToken = cancellationToken;
+        var configurationOptions = ConfigurationOptions.Parse("localhost");
+        configurationOptions.AbortOnConnectFail = false; // Allow retrying
+        var muxer = ConnectionMultiplexer.Connect(configurationOptions);
+        _db = muxer.GetDatabase();
+        Console.WriteLine("{RedisEventReader}: constructor called end");
+    }
+
+    public async Task ConsumeMessagesAsync()
+    {
+        Console.WriteLine("{RedisEventReader}: starting to consume messages");
+
+
+        if (!(await _db.KeyExistsAsync(StreamName)) ||
+            (await _db.StreamGroupInfoAsync(StreamName)).All(x=>x.Name!=GroupName))
+        {
+            await _db.StreamCreateConsumerGroupAsync(StreamName, GroupName, "0-0", true);
+        }
+        
+        var id = string.Empty;
+        while (!_cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    await _db.StreamAcknowledgeAsync(StreamName, GroupName, id);
+                    id = string.Empty;
+                }
+                var result = await _db.StreamReadGroupAsync(StreamName, GroupName, "pos-member", ">", 1);
+                if (result.Any())
+                {
+                    var streamEntry = result.First();
+                    id = streamEntry.Id;
+                    Console.WriteLine(streamEntry.ToString());
+                    //CALL CHAIN EVENT HANDLER
+                    // var parsedEvent = ParseEvent(streamEntry);
+                    // if (parsedEvent == null)
+                    //     continue;
+                    // await memberRepository.UpdateEntityAsync(parsedEvent);
+                }
+                await Task.Delay(1000, _cancellationToken);
+            }
+            catch (OperationCanceledException )
+            {
+                Console.WriteLine("Redis Event Reader stopped!");
+                break;
+            }
+        }
+    }
+}
