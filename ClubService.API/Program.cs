c370f4570d91f09de72009e7db6d8900d72c1c99
Michael Spiegel <michaelspiegel@live.at>
2024-06-01 00:58:46 +0200

    Created TennisClubDeletedEventHandler.cs and registered it in Program.cs.

--- ClubService.API/Program.cs
+++ ClubService.API/Program.cs
@@ -118,6 +118,7 @@ var memberReadModelRepository = services.GetRequiredService<IMemberReadModelRepo
 var subscriptionTierCreatedEventHandler = new SubscriptionTierCreatedEventHandler(subscriptionTierReadModelRepository);
 var tennisClubRegisteredEventHandler = new TennisClubRegisteredEventHandler(tennisClubReadModelRepository);
 var tennisClubLockedEventHandler = new TennisClubLockedEventHandler(tennisClubReadModelRepository);
+var tennisClubDeletedEventHandler = new TennisClubDeletedEventHandler(tennisClubReadModelRepository);
 var adminRegisteredEventHandler = new AdminRegisteredEventHandler(adminReadModelRepository);
 var memberRegisteredEventHandler = new MemberRegisteredEventHandler(memberReadModelRepository);
 
@@ -125,6 +126,7 @@ var memberRegisteredEventHandler = new MemberRegisteredEventHandler(memberReadMo
 chainEventHandler.RegisterEventHandler(subscriptionTierCreatedEventHandler);
 chainEventHandler.RegisterEventHandler(tennisClubRegisteredEventHandler);
 chainEventHandler.RegisterEventHandler(tennisClubLockedEventHandler);
+chainEventHandler.RegisterEventHandler(tennisClubDeletedEventHandler);
 chainEventHandler.RegisterEventHandler(adminRegisteredEventHandler);
 chainEventHandler.RegisterEventHandler(memberRegisteredEventHandler);
 
