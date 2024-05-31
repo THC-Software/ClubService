2a4229f226ef464dfab311bd8fbe0f63be24ded5
Marco Prescher <m.prescher@gantner-instruments.com>
2024-05-31 22:09:12 +0200

    Added MemberReadModelRepository and MemberRegisteredEventHandler

---
+++ ClubService.Application/EventHandlers/MemberEventHandlers/MemberRegisteredEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using ClubService.Application.Api;
+using ClubService.Domain.Event;
+using ClubService.Domain.Event.Member;
+using ClubService.Domain.ReadModel;
+using ClubService.Domain.Repository;
+
+namespace ClubService.Application.EventHandlers.MemberEventHandlers;
+
+public class MemberRegisteredEventHandler(IMemberReadModelRepository memberReadModelRepository) : IEventHandler
+{
+    public async Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
+    {
+        if (!Supports(domainEnvelope))
+        {
+            return;
+        }
+        
+        var memberReadModel = MemberReadModel.FromDomainEvent((MemberRegisteredEvent)domainEnvelope.EventData);
+        await memberReadModelRepository.Add(memberReadModel);
+    }
+    
+    private static bool Supports(DomainEnvelope<IDomainEvent> domainEnvelope)
+    {
+        return domainEnvelope.EventType.Equals(EventType.MEMBER_REGISTERED);
+    }
+}
