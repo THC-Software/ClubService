49e387f0aea03f173ee8dbae37bb769de10a9ea3
Adrian <adrian.essig@outlook.com>
2024-05-31 21:56:23 +0200

    Created AdminReadModel

---
+++ ClubService.Domain/ReadModel/AdminReadModel.cs
@@ -0,0 +1,43 @@
+﻿using ClubService.Domain.Event.Admin;
+using ClubService.Domain.Model.Enum;
+using ClubService.Domain.Model.ValueObject;
+
+namespace ClubService.Domain.ReadModel;
+
+public class AdminReadModel
+{
+    public AdminId AdminId { get; }
+    public string Username { get; }
+    public FullName Name { get; }
+    public TennisClubId TennisClubId { get; }
+    public AdminStatus Status { get; }
+    
+    private AdminReadModel()
+    {
+    } // needed by efcore
+    
+    private AdminReadModel(
+        AdminId adminId,
+        string username,
+        FullName name,
+        TennisClubId tennisClubId,
+        AdminStatus status)
+    {
+        AdminId = adminId;
+        Username = username;
+        Name = name;
+        TennisClubId = tennisClubId;
+        Status = status;
+    }
+    
+    public static AdminReadModel FromDomainEvent(AdminRegisteredEvent adminRegisteredEvent)
+    {
+        return new AdminReadModel(
+            adminRegisteredEvent.AdminId,
+            adminRegisteredEvent.Username,
+            adminRegisteredEvent.Name,
+            adminRegisteredEvent.TennisClubId,
+            adminRegisteredEvent.Status
+        );
+    }
+}
