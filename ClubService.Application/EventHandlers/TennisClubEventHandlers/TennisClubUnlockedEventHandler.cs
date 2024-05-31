47cfa1215dcc823c4decda555bbb55f911d71035
Michael Spiegel <michaelspiegel@live.at>
2024-06-01 00:34:26 +0200

    Created TennisClubUnlockedEventHandler.cs and registered it in Program.cs.

---
+++ ClubService.Application/EventHandlers/TennisClubEventHandlers/TennisClubUnlockedEventHandler.cs
@@ -0,0 +1,14 @@
+using ClubService.Application.Api;
+using ClubService.Domain.Event;
+using ClubService.Domain.Repository;
+
+namespace ClubService.Application.EventHandlers.TennisClubEventHandlers;
+
+public class TennisClubUnlockedEventHandler(ITennisClubReadModelRepository tennisClubReadModelRepository)
+    : IEventHandler
+{
+    public Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
+    {
+        throw new NotImplementedException();
+    }
+}
