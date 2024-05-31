d7e5a192a860b00d8db9a7d1c2d2add5a0070aa3
Marco Prescher <m.prescher@gantner-instruments.com>
2024-05-31 21:11:38 +0200

    Added member read model

---
+++ ClubService.Domain/ReadModel/MemberReadModel.cs
@@ -0,0 +1,39 @@
+﻿using ClubService.Domain.Event.Member;
+using ClubService.Domain.Model.Enum;
+using ClubService.Domain.Model.ValueObject;
+
+namespace ClubService.Domain.ReadModel;
+
+public class MemberReadModel
+{
+    public MemberId MemberId { get; }
+    public FullName Name { get; }
+    public string Email { get; }
+    public TennisClubId TennisClubId { get; }
+    public MemberStatus Status { get; }
+    
+    private MemberReadModel(
+        MemberId memberId,
+        FullName name,
+        string email,
+        TennisClubId tennisClubId,
+        MemberStatus status)
+    {
+        MemberId = memberId;
+        Name = name;
+        Email = email;
+        TennisClubId = tennisClubId;
+        Status = status;
+    }
+    
+    public static MemberReadModel FromDomainEvent(MemberRegisteredEvent memberRegisteredEvent)
+    {
+        return new MemberReadModel(
+            memberRegisteredEvent.MemberId,
+            memberRegisteredEvent.Name,
+            memberRegisteredEvent.Email,
+            memberRegisteredEvent.TennisClubId,
+            memberRegisteredEvent.Status
+        );
+    }
+}
