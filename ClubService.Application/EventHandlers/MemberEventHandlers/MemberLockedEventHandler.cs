cfeea1423a02f4670fe0a829c13167ecd974aec0
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-01 21:39:11 +0200

    added MemberLockedEventHandler functionalities and implemented MemberReadModel lock function

--- ClubService.Application/EventHandlers/MemberEventHandlers/MemberLockedEventHandler.cs
+++ ClubService.Application/EventHandlers/MemberEventHandlers/MemberLockedEventHandler.cs
@@ -6,8 +6,29 @@ namespace ClubService.Application.EventHandlers.MemberEventHandlers;
 
 public class MemberLockedEventHandler(IMemberReadModelRepository memberReadModelRepository) : IEventHandler
 {
-    public Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
+    public async Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
     {
-        throw new NotImplementedException();
+        if (!Supports(domainEnvelope))
+        {
+            return;
+        }
+        
+        var memberReadModel = await memberReadModelRepository.GetMemberById(domainEnvelope.EntityId);
+        
+        if (memberReadModel != null)
+        {
+            memberReadModel.Lock();
+            await memberReadModelRepository.Update();
+        }
+        else
+        {
+            // TODO: Add logging
+            Console.WriteLine($"Could not lock member with id {domainEnvelope.EntityId}!");
+        }
+    }
+    
+    private static bool Supports(DomainEnvelope<IDomainEvent> domainEnvelope)
+    {
+        return domainEnvelope.EventType.Equals(EventType.MEMBER_LOCKED);
     }
 }
