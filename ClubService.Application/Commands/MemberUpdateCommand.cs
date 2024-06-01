89032dd546b12a9870af1ddf8c4edc555dc2d67a
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-01 23:52:33 +0200

    Updated MemberUpdateCommand

--- ClubService.Application/Commands/MemberUpdateCommand.cs
+++ ClubService.Application/Commands/MemberUpdateCommand.cs
@@ -1,5 +1,10 @@
 namespace ClubService.Application.Commands;
 
-public class MemberUpdateCommand
+public class MemberUpdateCommand(string? firstName, string? lastName, string? email)
 {
+    public string? FirstName { get; } = firstName;
+    
+    public string? LastName { get; } = lastName;
+    
+    public string? Email { get; } = email;
 }
