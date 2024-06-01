551be44e381d49dc911ba00348b3b19806c661f8
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-01 17:40:10 +0200

    Added transaction manager

---
+++ ClubService.Domain/Repository/IReadStoreTransactionManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ClubService.Domain.Repository;
+
+public interface IReadStoreTransactionManager : IDisposable
+{
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
---
+++ ClubService.Infrastructure/ReadStoreReadStoreTransactionManager.cs
@@ -0,0 +1,48 @@
+﻿using ClubService.Domain.Repository;
+using ClubService.Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ClubService.Infrastructure;
+
+public class ReadStoreReadStoreTransactionManager(ReadStoreDbContext readStoreDbContext) : IReadStoreTransactionManager
+{
+    private IDbContextTransaction? _transaction;
+    
+    public async Task BeginTransactionAsync()
+    {
+        _transaction = await readStoreDbContext.Database.BeginTransactionAsync();
+    }
+    
+    public async Task CommitTransactionAsync()
+    {
+        if (_transaction != null)
+        {
+            await _transaction.CommitAsync();
+            await DisposeTransactionAsync();
+        }
+    }
+    
+    public async Task RollbackTransactionAsync()
+    {
+        if (_transaction != null)
+        {
+            await _transaction.RollbackAsync();
+            await DisposeTransactionAsync();
+        }
+    }
+    
+    private async Task DisposeTransactionAsync()
+    {
+        if (_transaction != null)
+        {
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+    }
+    
+    public void Dispose()
+    {
+        _transaction?.Dispose();
+        _transaction = null;
+    }
+}
