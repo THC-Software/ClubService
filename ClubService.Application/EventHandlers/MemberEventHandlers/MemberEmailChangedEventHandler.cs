d495cc80aab05d7f9d91f9af5a2e3892ee316a30
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-03 20:11:43 +0200

    Implemented MemberEmailChangedEventHandler Handle function

--- ClubService.Application/EventHandlers/MemberEventHandlers/MemberEmailChangedEventHandler.cs
+++ ClubService.Application/EventHandlers/MemberEventHandlers/MemberEmailChangedEventHandler.cs
@@ -1,13 +1,35 @@
 ﻿using ClubService.Application.Api;
 using ClubService.Domain.Event;
+using ClubService.Domain.Event.Member;
 using ClubService.Domain.Repository;
 
 namespace ClubService.Application.EventHandlers.MemberEventHandlers;
 
 public class MemberEmailChangedEventHandler(IMemberReadModelRepository memberReadModelRepository) : IEventHandler
 {
-    public Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
+    public async Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
     {
-        throw new NotImplementedException();
+        if (!Supports(domainEnvelope))
+        {
+            return;
+        }
+        
+        var memberEmailChangedEvent = (MemberEmailChangedEvent)domainEnvelope.EventData;
+        var memberReadModel = await memberReadModelRepository.GetMemberById(domainEnvelope.EntityId);
+        
+        if (memberReadModel == null)
+        {
+            // TODO: Add logging
+            Console.WriteLine($"Member with id {domainEnvelope.EntityId} not found!");
+            return;
+        }
+        
+        memberReadModel.ChangeEmail(memberEmailChangedEvent.Email);
+        await memberReadModelRepository.Update();
+    }
+    
+    private static bool Supports(DomainEnvelope<IDomainEvent> domainEnvelope)
+    {
+        return domainEnvelope.EventType.Equals(EventType.MEMBER_EMAIL_CHANGED);
     }
 }
