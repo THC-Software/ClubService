c7236dd31b9a3563ca8f99f5bb52e9ae57ad55e2
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-03 20:26:03 +0200

    Added MemberLimitExceededException

---
+++ ClubService.Application/Api/Exceptions/MemberLimitExceededException.cs
@@ -0,0 +1,6 @@
+﻿namespace ClubService.Application.Api.Exceptions;
+
+public class MemberLimitExceededException(int maxMemberCount) : Exception
+{
+    public override string Message { get; } = $"Member limit of {maxMemberCount} exceeded.";
+}
