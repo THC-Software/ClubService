3a8cf40117e8927357bdfd504d5c712c27cee8e8
Marco Prescher <mpr8243@students.fhv.at>
2024-06-05 15:27:44 +0200

    Added AdminUsernameAlreadyExist exception

---
+++ ClubService.Application/Api/Exceptions/AdminUsernameAlreadyExist.cs
@@ -0,0 +1,7 @@
+﻿namespace ClubService.Application.Api.Exceptions;
+
+public class AdminUsernameAlreadyExist(string username, string tennisClubName, Guid tennisClubId) : Exception
+{
+    public override string Message { get; } =
+        $"Admin username '{username}' already exist in tennis club '{tennisClubName}' ({tennisClubId}).";
+}
