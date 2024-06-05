5e1cc34928cc0d03324f81764460cc5163de1d41
Marco Prescher <mpr8243@students.fhv.at>
2024-06-05 12:59:08 +0200

    Added tennis club status check in DeleteMemberService

--- ClubService.Application/Impl/DeleteMemberService.cs
+++ ClubService.Application/Impl/DeleteMemberService.cs
@@ -1,8 +1,9 @@
-﻿using System.Data;
-using ClubService.Application.Api;
+﻿using ClubService.Application.Api;
 using ClubService.Application.Api.Exceptions;
 using ClubService.Domain.Event.Member;
+using ClubService.Domain.Event.TennisClub;
 using ClubService.Domain.Model.Entity;
+using ClubService.Domain.Model.Enum;
 using ClubService.Domain.Model.ValueObject;
 using ClubService.Domain.Repository;
 using ClubService.Domain.Repository.Transaction;
@@ -29,25 +30,44 @@ public class DeleteMemberService(
             member.Apply(domainEvent);
         }
         
-        try
+        var tennisClubDomainEvents =
+            await eventRepository.GetEventsForEntity<ITennisClubDomainEvent>(member.TennisClubId.Id);
+        
+        var tennisClub = new TennisClub();
+        foreach (var domainEvent in tennisClubDomainEvents)
         {
-            var domainEvents = member.ProcessMemberDeleteCommand();
-            var expectedEventCount = existingMemberDomainEvents.Count;
-            
-            await eventStoreTransactionManager.TransactionScope(async () =>
-            {
-                foreach (var domainEvent in domainEvents)
-                {
-                    member.Apply(domainEvent);
-                    expectedEventCount = await eventRepository.Append(domainEvent, expectedEventCount);
-                }
-            });
-        }
-        catch (InvalidOperationException ex)
-        {
-            throw new ConflictException(ex.Message, ex);
+            tennisClub.Apply(domainEvent);
         }
         
-        return id;
+        switch (tennisClub.Status)
+        {
+            case TennisClubStatus.ACTIVE:
+                try
+                {
+                    var domainEvents = member.ProcessMemberDeleteCommand();
+                    var expectedEventCount = existingMemberDomainEvents.Count;
+                    
+                    await eventStoreTransactionManager.TransactionScope(async () =>
+                    {
+                        foreach (var domainEvent in domainEvents)
+                        {
+                            member.Apply(domainEvent);
+                            expectedEventCount = await eventRepository.Append(domainEvent, expectedEventCount);
+                        }
+                    });
+                }
+                catch (InvalidOperationException ex)
+                {
+                    throw new ConflictException(ex.Message, ex);
+                }
+                
+                return id;
+            case TennisClubStatus.LOCKED:
+                throw new ConflictException("Tennis club is locked!");
+            case TennisClubStatus.DELETED:
+                throw new ConflictException("Tennis club already deleted!");
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
