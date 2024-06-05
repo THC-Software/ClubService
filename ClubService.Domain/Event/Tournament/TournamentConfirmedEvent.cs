38176c0dca2c740b1132fbcd2cd33c0f0dde2b09
Michael Spiegel <michaelspiegel@live.at>
2024-06-05 08:21:33 +0200

    Added TournamentStatus.cs and updated TournamentConfirmedEvent.cs to be up to date with the event in the TournamentService.

--- ClubService.Domain/Event/Tournament/TournamentConfirmedEvent.cs
+++ ClubService.Domain/Event/Tournament/TournamentConfirmedEvent.cs
@@ -1,19 +1,30 @@
 using ClubService.Domain.Model.Entity;
+using ClubService.Domain.Model.Enum;
 
 namespace ClubService.Domain.Event.Tournament;
 
 public class TournamentConfirmedEvent(
     Guid id,
+    Guid clubId,
     bool isCanceled,
+    string name,
+    string description,
     int maximumParticipants,
     HashSet<TournamentCourt> courts,
     HashSet<TournamentDay> days,
-    HashSet<TournamentParticipant> participants) : ITournamentDomainEvent
+    HashSet<TournamentParticipant> participants,
+    int version,
+    TournamentStatus status) : ITournamentDomainEvent
 {
     public Guid Id { get; } = id;
+    public Guid ClubId { get; } = clubId;
     public bool IsCanceled { get; } = isCanceled;
+    public string Name { get; } = name;
+    public string Description { get; } = description;
     public int MaximumParticipants { get; } = maximumParticipants;
     public HashSet<TournamentCourt> Courts { get; } = courts;
-    private HashSet<TournamentDay> Days { get; } = days;
-    private HashSet<TournamentParticipant> Participants { get; } = participants;
+    public HashSet<TournamentDay> Days { get; } = days;
+    public HashSet<TournamentParticipant> Participants { get; } = participants;
+    public int Version { get; } = version;
+    public TournamentStatus Status { get; } = status;
 }
