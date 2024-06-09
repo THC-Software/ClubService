d586b75d29d0d30f2d530c85fc03f7ae545f4aa7
Adrian <Adrian.Essig@outlook.com>
2024-06-09 21:38:08 +0200

    Made constructor private and created "Create" method that needs the PasswordHasherService, so that we can't forget to hash the password

--- ClubService.Domain/Model/Entity/UserPassword.cs
+++ ClubService.Domain/Model/Entity/UserPassword.cs
@@ -1,3 +1,4 @@
+using ClubService.Domain.Api;
 using ClubService.Domain.Model.ValueObject;
 
 namespace ClubService.Domain.Model.Entity;
@@ -5,18 +6,22 @@ public class UserPassword
 {
     public UserId UserId;
     public string HashedPassword;
-
     // needed by efcore
     private UserPassword()
     {
         
     }
 
-    public UserPassword(Guid userId, string password)
+    private UserPassword(Guid userId, string hashedPassword)
     {
  
         UserId = new UserId(userId);
-        // TODO: Hash password
-        HashedPassword = password;
+        HashedPassword = hashedPassword;
+    }
+
+    public static UserPassword Create(Guid userId, string password, IPasswordHasherService passwordHasherService)
+    {
+        string hashedPassword = passwordHasherService.HashPassword(password);
+        return new UserPassword(userId, hashedPassword);
     }
 }
