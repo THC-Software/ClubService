f0cceed7855f6bf441aaa86648bf10f928327582
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-12 15:53:46 +0200

    Removed ChangeFullName and ChangeEmail in UpdateMemberService.cs

--- ClubService.Application/Api/IUpdateMemberService.cs
+++ ClubService.Application/Api/IUpdateMemberService.cs
@@ -6,7 +6,5 @@ public interface IUpdateMemberService
 {
     Task<Guid> LockMember(Guid id);
     Task<Guid> UnlockMember(Guid id);
-    Task<Guid> ChangeFullName(Guid id, string firstName, string lastName);
-    Task<Guid> ChangeEmail(Guid id, string email);
     Task<Guid> UpdateMember(Guid id, MemberUpdateCommand memberUpdateCommand);
 }
