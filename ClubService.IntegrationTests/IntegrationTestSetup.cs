99b69257d2ade94a75c1579769216e3086186bd0
Michael Spiegel <michaelspiegel@live.at>
2024-06-13 16:45:08 +0200

    Moved everything from TestBase.cs that needs to be set up before all the test to IntegrationTestSetup.cs.

--- ClubService.IntegrationTests/IntegrationTestSetup.cs
+++ ClubService.IntegrationTests/IntegrationTestSetup.cs
@@ -1,3 +1,7 @@
+using ClubService.Domain.Repository;
+using ClubService.Infrastructure.DbContexts;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Testcontainers.PostgreSql;
 
 namespace ClubService.IntegrationTests;
@@ -5,14 +9,28 @@ namespace ClubService.IntegrationTests;
 [SetUpFixture]
 public static class IntegrationTestSetup
 {
-    public static PostgreSqlContainer PostgresContainer { get; private set; }
+    private static PostgreSqlContainer _postgresContainer = null!;
+    private static WebAppFactory _webAppFactory = null!;
+    public static EventStoreDbContext EventStoreDbContext { get; private set; } = null!;
+    public static IEventRepository EventRepository { get; private set; } = null!;
+    public static HttpClient HttpClient { get; private set; } = null!;
+    public static Mock<IAdminReadModelRepository> MockAdminReadModelRepository { get; private set; } = null!;
+    public static Mock<IMemberReadModelRepository> MockMemberReadModelRepository { get; private set; } = null!;
+
+    public static Mock<ISubscriptionTierReadModelRepository> MockSubscriptionTierReadModelRepository
+    {
+        get;
+        private set;
+    } = null!;
+
+    public static Mock<ITennisClubReadModelRepository> MockTennisClubReadModelRepository { get; private set; } = null!;
 
     [OneTimeSetUp]
     public static async Task OneTimeSetup()
     {
         Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Test");
 
-        PostgresContainer = new PostgreSqlBuilder()
+        _postgresContainer = new PostgreSqlBuilder()
             .WithImage("debezium/postgres:16-alpine")
             .WithUsername("user")
             .WithPassword("password")
@@ -20,12 +38,42 @@ public static class IntegrationTestSetup
             .WithPortBinding(5432, true)
             .Build();
 
-        await PostgresContainer.StartAsync();
+        await _postgresContainer.StartAsync();
+
+        // mock repositories for write side integration tests
+        MockTennisClubReadModelRepository = new Mock<ITennisClubReadModelRepository>();
+        MockSubscriptionTierReadModelRepository = new Mock<ISubscriptionTierReadModelRepository>();
+        MockAdminReadModelRepository = new Mock<IAdminReadModelRepository>();
+        MockMemberReadModelRepository = new Mock<IMemberReadModelRepository>();
+
+        _webAppFactory = new WebAppFactory(
+            _postgresContainer.GetConnectionString(),
+            MockTennisClubReadModelRepository,
+            MockSubscriptionTierReadModelRepository,
+            MockAdminReadModelRepository,
+            MockMemberReadModelRepository
+        );
+
+        HttpClient = _webAppFactory.CreateClient();
+
+        var scopeFactory = _webAppFactory.Services.GetService<IServiceScopeFactory>() ??
+                           throw new Exception("Scope factory not found");
+        var scope = scopeFactory.CreateScope() ??
+                    throw new Exception("Could not create Scope");
+
+        EventStoreDbContext = scope.ServiceProvider.GetService<EventStoreDbContext>() ??
+                              throw new Exception("Could not get ApplicationDbContext");
+        EventRepository = scope.ServiceProvider.GetService<IEventRepository>() ??
+                          throw new Exception("Could not get EventRepository");
     }
 
     [OneTimeTearDown]
     public static async Task OneTimeTearDown()
     {
-        await PostgresContainer.StopAsync();
+        await EventStoreDbContext.DisposeAsync();
+        await _postgresContainer.StopAsync();
+        await _postgresContainer.DisposeAsync();
+        HttpClient.Dispose();
+        await _webAppFactory.DisposeAsync();
     }
 }
--- ClubService.IntegrationTests/TestBase.cs
+++ ClubService.IntegrationTests/TestBase.cs
@@ -1,57 +1,34 @@
 using ClubService.Domain.Repository;
-using ClubService.Infrastructure.DbContexts;
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
 namespace ClubService.IntegrationTests;
 
 public class TestBase
 {
-    private EventStoreDbContext _eventStoreDbContext;
-    private WebAppFactory _factory;
-    protected IEventRepository EventRepository;
-    protected HttpClient HttpClient;
-    protected Mock<IAdminReadModelRepository> MockAdminReadModelRepository;
-    protected Mock<IMemberReadModelRepository> MockMemberReadModelRepository;
-    protected Mock<ISubscriptionTierReadModelRepository> MockSubscriptionTierReadModelRepository;
-    protected Mock<ITennisClubReadModelRepository> MockTennisClubReadModelRepository;
+    protected readonly IEventRepository EventRepository = IntegrationTestSetup.EventRepository;
+    protected readonly HttpClient HttpClient = IntegrationTestSetup.HttpClient;
+
+    protected readonly Mock<IAdminReadModelRepository> MockAdminReadModelRepository =
+        IntegrationTestSetup.MockAdminReadModelRepository;
+
+    protected readonly Mock<IMemberReadModelRepository> MockMemberReadModelRepository =
+        IntegrationTestSetup.MockMemberReadModelRepository;
+
+    protected readonly Mock<ISubscriptionTierReadModelRepository> MockSubscriptionTierReadModelRepository =
+        IntegrationTestSetup.MockSubscriptionTierReadModelRepository;
+
+    protected readonly Mock<ITennisClubReadModelRepository> MockTennisClubReadModelRepository =
+        IntegrationTestSetup.MockTennisClubReadModelRepository;
 
     [SetUp]
     public async Task Setup()
     {
-        // mock repositories for write side integration tests
-        MockTennisClubReadModelRepository = new Mock<ITennisClubReadModelRepository>();
-        MockSubscriptionTierReadModelRepository = new Mock<ISubscriptionTierReadModelRepository>();
-        MockAdminReadModelRepository = new Mock<IAdminReadModelRepository>();
-        MockMemberReadModelRepository = new Mock<IMemberReadModelRepository>();
-
-        _factory = new WebAppFactory(
-            IntegrationTestSetup.PostgresContainer.GetConnectionString(),
-            MockTennisClubReadModelRepository,
-            MockSubscriptionTierReadModelRepository,
-            MockAdminReadModelRepository,
-            MockMemberReadModelRepository
-        );
-        HttpClient = _factory.CreateClient();
-
-        var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>() ??
-                           throw new Exception("Scope factory not found");
-        var scope = scopeFactory.CreateScope() ??
-                    throw new Exception("Could not create Scope");
-
-        _eventStoreDbContext = scope.ServiceProvider.GetService<EventStoreDbContext>() ??
-                               throw new Exception("Could not get ApplicationDbContext");
-        EventRepository = scope.ServiceProvider.GetService<IEventRepository>() ??
-                          throw new Exception("Could not get EventRepository");
-        await _eventStoreDbContext.Database.EnsureCreatedAsync();
+        await IntegrationTestSetup.EventStoreDbContext.Database.EnsureCreatedAsync();
     }
 
     [TearDown]
     public async Task TearDown()
     {
-        await _eventStoreDbContext.Database.EnsureDeletedAsync();
-        HttpClient.Dispose();
-        await _factory.DisposeAsync();
-        await _eventStoreDbContext.DisposeAsync();
+        await IntegrationTestSetup.EventStoreDbContext.Database.EnsureDeletedAsync();
     }
 }
