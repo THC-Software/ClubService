79e144eadcd01736df89b60fe1d0a32ebf02d91a
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-15 15:30:21 +0200

    added comment + minor code formatting

--- ClubService.Application/Impl/DeleteTennisClubService.cs
+++ ClubService.Application/Impl/DeleteTennisClubService.cs
@@ -16,30 +16,30 @@ public class DeleteTennisClubService(
     public async Task<Guid> DeleteTennisClub(Guid id)
     {
         loggerService.LogDeleteTennisClub(id);
-
+        
         var tennisClubId = new TennisClubId(id);
-        var tennisClub = new TennisClub();
-
+        
         var existingTennisClubDomainEvents =
             await eventRepository.GetEventsForEntity<ITennisClubDomainEvent>(tennisClubId.Id);
-
+        
         if (existingTennisClubDomainEvents.Count == 0)
         {
             loggerService.LogTennisClubNotFound(id);
             throw new TennisClubNotFoundException(tennisClubId.Id);
         }
-
+        
+        var tennisClub = new TennisClub();
         foreach (var domainEvent in existingTennisClubDomainEvents)
         {
             tennisClub.Apply(domainEvent);
         }
-
-        //TODO: Delete all members and admins?
+        
+        // we decided to only delete the corresponding admins and members on the read side
         try
         {
             var domainEvents = tennisClub.ProcessTennisClubDeleteCommand();
             var expectedEventCount = existingTennisClubDomainEvents.Count;
-
+            
             await eventStoreTransactionManager.TransactionScope(async () =>
             {
                 foreach (var domainEvent in domainEvents)
@@ -54,7 +54,7 @@ public class DeleteTennisClubService(
             loggerService.LogInvalidOperationException(ex);
             throw new ConflictException(ex.Message, ex);
         }
-
+        
         loggerService.LogTennisClubDeleted(id);
         return tennisClub.TennisClubId.Id;
     }
