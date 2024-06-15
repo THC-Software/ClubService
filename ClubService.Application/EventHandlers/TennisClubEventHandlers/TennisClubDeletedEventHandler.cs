a285fdcfcfb24c6fa8b6c24f8e8da79b9b211582
Marco Prescher <m.prescher@gantner-instruments.com>
2024-06-15 15:13:28 +0200

    added functionality to delete all members and admins on tennis club delete in TennisClubDeletedEventHandler.cs

--- ClubService.Application/EventHandlers/TennisClubEventHandlers/TennisClubDeletedEventHandler.cs
+++ ClubService.Application/EventHandlers/TennisClubEventHandlers/TennisClubDeletedEventHandler.cs
@@ -1,11 +1,15 @@
 using ClubService.Application.Api;
 using ClubService.Domain.Event;
 using ClubService.Domain.Repository;
+using ClubService.Domain.Repository.Transaction;
 
 namespace ClubService.Application.EventHandlers.TennisClubEventHandlers;
 
 public class TennisClubDeletedEventHandler(
     ITennisClubReadModelRepository tennisClubReadModelRepository,
+    IAdminReadModelRepository adminReadModelRepository,
+    IMemberReadModelRepository memberReadModelRepository,
+    IReadStoreTransactionManager readStoreTransactionManager,
     ILoggerService<TennisClubDeletedEventHandler> loggerService) : IEventHandler
 {
     public async Task Handle(DomainEnvelope<IDomainEvent> domainEnvelope)
@@ -15,21 +19,27 @@ public class TennisClubDeletedEventHandler(
             loggerService.LogRejectEvent(domainEnvelope);
             return;
         }
-
+        
         loggerService.LogHandleEvent(domainEnvelope);
-
+        
         var tennisClubReadModel = await tennisClubReadModelRepository.GetTennisClubById(domainEnvelope.EntityId);
-
+        
         if (tennisClubReadModel == null)
         {
             loggerService.LogTennisClubNotFound(domainEnvelope.EntityId);
             return;
         }
-
-        await tennisClubReadModelRepository.Delete(tennisClubReadModel);
+        
+        await readStoreTransactionManager.TransactionScope(async () =>
+        {
+            await memberReadModelRepository.DeleteMembersByTennisClubId(tennisClubReadModel.TennisClubId.Id);
+            await adminReadModelRepository.DeleteAdminsByTennisClubId(tennisClubReadModel.TennisClubId.Id);
+            await tennisClubReadModelRepository.Delete(tennisClubReadModel);
+        });
+        
         loggerService.LogTennisClubDeleted(tennisClubReadModel.TennisClubId.Id);
     }
-
+    
     private static bool Supports(DomainEnvelope<IDomainEvent> domainEnvelope)
     {
         return domainEnvelope.EventType.Equals(EventType.TENNIS_CLUB_DELETED);
