0977a73c3130d2644b87901a06ed24e2b2544385
Marco Prescher <mpr8243@students.fhv.at>
2024-06-25 17:19:13 +0200

    Added TournamentNotFoundException.cs

---
+++ ClubService.Application/Api/Exceptions/TournamentNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace ClubService.Application.Api.Exceptions;
+
+public class TournamentNotFoundException(Guid id) : Exception
+{
+    public override string Message { get; } = $"Tournament with id {id} not found.";
+}
