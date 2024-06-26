2202f404d82127a1cafdeac0d686d4d49b7aa809
Marco Prescher <mpr8243@students.fhv.at>
2024-06-26 19:10:20 +0200

    Added AuthenticationExtensions.cs

---
+++ ClubService.API/ApplicationConfigurations/AuthenticationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ClubService.API.ApplicationConfigurations;
+
+public static class AuthenticationExtensions
+{
+    public static void AddAuthenticationConfigurations(this IServiceCollection service, IConfiguration configuration)
+    {
+        service
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = false,
+                    RequireSignedTokens = false,
+                    SaveSigninToken = true,
+                    RoleClaimType = "groups"
+                };
+            });
+
+        service.AddAuthorizationBuilder()
+            .AddPolicy("AdminPolicy", policy => policy.RequireRole("ADMIN"))
+            .AddPolicy("MemberPolicy", policy => policy.RequireRole("MEMBER"));
+    }
+}
