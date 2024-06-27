282530e757135211732ce253b4118899768ec4ff
Marco Prescher <mpr8243@students.fhv.at>
2024-06-27 17:04:25 +0200

    Added BasicAuthenticationHandler, so that we can ensure that the authorization middleware works correctly and returns the appropriate 403 Forbidden response

---
+++ ClubService.API/BasicAuthenticationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace ClubService.API;
+
+public class BasicAuthenticationHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    ISystemClock clock)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder, clock)
+{
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[] { new Claim(ClaimTypes.Name, "DummyUser") };
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
--- ClubService.API/Program.cs
+++ ClubService.API/Program.cs
@@ -1,6 +1,7 @@
 using ClubService.API;
 using ClubService.API.ApplicationConfigurations;
 using ClubService.Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,15 @@ builder.Services.AddApiVersioningConfigurations();
 builder.Services.AddSwaggerConfigurations();
 builder.Services.AddExceptionHandlerConfigurations();
 builder.Services.AddControllers();
+
+// By configuring a default authentication scheme, you can ensure that the authorization middleware works correctly
+// and returns the appropriate 403 Forbidden response when the user does not have the required role.
+// The custom middleware continues to handle the extraction of claims from the JWT, while the dummy
+// authentication scheme satisfies the requirement for a default authentication handler.
+builder.Services
+    .AddAuthentication("BasicScheme")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicScheme", null);
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
@@ -40,10 +50,9 @@ if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("DockerDeve
     await loginStoreDbContext.Database.EnsureCreatedAsync();
 }
 
-app.UseHttpsRedirection();
-
 app.UseExceptionHandler();
 app.UseMiddleware<JwtClaimsMiddleware>(); // Use custom middleware to extract JWT claims
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
