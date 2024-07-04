82e4473c552462248034fea5757bffc026f32647
Michael Spiegel <michaelspiegel@live.at>
2024-07-04 19:08:34 +0200

    Added migration for event store.

---
+++ ClubService.Infrastructure/Migrations/20240704170701_InitialEventStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialEventStore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DomainEvent",
+                columns: table => new
+                {
+                    eventId = table.Column<Guid>(type: "uuid", nullable: false),
+                    entityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    eventType = table.Column<string>(type: "text", nullable: false),
+                    entityType = table.Column<string>(type: "text", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    eventData = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pK_DomainEvent", x => x.eventId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DomainEvent");
+        }
+    }
+}
