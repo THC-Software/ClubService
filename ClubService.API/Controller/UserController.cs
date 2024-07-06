6d7a0f53600563adf8a5b3c27fcbcd89f672ea00
Michael Spiegel <michaelspiegel@live.at>
2024-07-06 13:53:40 +0200

    Fixed api paths in UserController.cs.

--- ClubService.API/Controller/UserController.cs
+++ ClubService.API/Controller/UserController.cs
@@ -7,12 +7,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ClubService.API.Controller;
 
-[Route("api/v{version:apiVersion}")]
+[Route("api/v{version:apiVersion}/users")]
 [ApiController]
 [ApiVersion("1.0")]
 public class UserController(ILoginService loginService, IUserService userService) : ControllerBase
 {
-    [HttpPost("/login")]
+    [HttpPost("login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -24,14 +24,14 @@ public class UserController(ILoginService loginService, IUserService userService
         return Ok(userInformationDto);
     }
 
-    [HttpPatch("/changePassword")]
+    [HttpPatch("changePassword")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    [Authorize(Roles = "ADMIN,MEMBER")]
+    [Authorize(Roles = "SYSTEM_OPERATOR,ADMIN,MEMBER")]
     public async Task<ActionResult> ChangePassword([FromBody] ChangePasswordCommand changePasswordCommand)
     {
         var jwtUserId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
