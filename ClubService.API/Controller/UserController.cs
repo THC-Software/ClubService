00b2adc7681e34d37bd2444c9d70a499dac02340
Michael Spiegel <michaelspiegel@live.at>
2024-07-06 19:06:10 +0200

    Added Unauthorized annotation to Login endpoint in UserController.cs.

--- ClubService.API/Controller/UserController.cs
+++ ClubService.API/Controller/UserController.cs
@@ -14,6 +14,7 @@ public class UserController(ILoginService loginService, IUserService userService
 {
     [HttpPost("login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
